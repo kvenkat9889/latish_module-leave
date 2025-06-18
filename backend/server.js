ab6a9bf3ce516bd5068a78b1bf8a51630285d868
kvenkat9889 <kvenkat9889@gmail.com>
2025-06-18 15:19:48 +0530

    new ip

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = 3004;
 
 // Middleware
 app.use(cors());
